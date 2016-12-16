72169d67213dbe824df0108c826e4bfea1679417
adrianke77 <adrianke77@gmail.com>
2016-12-16 16:56:48 +0800

    done

--- src/Car.js
+++ src/Car.js
@@ -1,22 +1,65 @@
-function Car(make, model, year, color, seats) {
-  this.make = make;
-  this.model = model;
-  this.year = year;
-  // TODO: add color, seats here
+function Car (make, model, year, color, seats, passengers) {
+  this.make = make
+  this.model = model
+  this.year = year
+  this.color = color
+  this.seats = seats
+  this.previousOwners = []
+  this.owner = "manufacturer"
+  this.running = false
+  this.sell = function (newOwner) {
+    this.previousOwners.push(this.owner)
+    this.owner = newOwner
+  }
+  this.paint = function (newColor) {
+    this.color = newColor
+  }
+  this.start = function () {
+    this.running = true
+  }
+  this.off = function () {
+    this.running = false
+  }
+  this.driveTo = function (destination) {
+    if (this.running === true) {
+      console.log("driving to " + destination)
+      return true
+    }
+    return false
+  }
+  this.park = function () {
+    if (this.running === false) {
+      console.log("parked")
+      return true
+    }
+    return false
+  }
 
-  this.running = false;
-  this.owner = 'manufacturer';
-  this.previousOwners = [];
-}
+  console.log(typeof passengers)
+  if (typeof passengers === "undefined") this.passengers = []
+    else this.passengers = passengers
 
-Car.prototype.sell = function(newOwner) {
-  return newOwner;
-};
+    
+  this.pickUp = function (name) {
+    if (this.running === true && passengers.length < seats -1 ) {
+      passengers.push(name)
+      return true
+    }
+    return false
+  }  
+  this.dropOff = function (name) {
+    if (this.passengers.indexOf(name) > -1 && this.running === true) {
+      passengers.splice(passengers.indexOf(name),1)
+      console.log("driving to drop off " + name)
+      return true
+    }
+    return false
+  }
+  this.passengerCount = function () {
+    return passengers.length
+  }
+} 
 
-Car.prototype.paint = function(newColor) {
-  return newColor;
-};
 
-// export the Car function for use in node //
-// this is required for the test.js to load this //
-module.exports = Car;
+
+module.exports = Car
